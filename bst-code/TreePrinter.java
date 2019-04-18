1546dd89a942288916c5c584589eccb9b9a51f53
Afnaan Karimi <24482619+afnaank@users.noreply.github.com>
2019-04-18 00:27:36 -0400

    Update TreePrinter

--- bst-code/TreePrinter.java
+++ bst-code/TreePrinter.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-class TreePrinter {
 
-    public static <T extends Comparable<?>> void printNode(BinaryNode<T> root) {
+public class TreePrinter 
+{
+    public static <T extends Comparable<?>> void printNode(BinaryNode<T> root) 
+    {
         int maxLevel = TreePrinter.maxLevel(root);
 
         printNodeInternal(Collections.singletonList(root), 1, maxLevel);
     }
 
-    private static <T extends Comparable<?>> void printNodeInternal(List<BinaryNode<T>> nodes, int level, int maxLevel) {
+    private static <T extends Comparable<?>> void printNodeInternal(List<BinaryNode<T>> nodes, int level, int maxLevel) 
+    {
         if (nodes.isEmpty() || TreePrinter.isAllElementsNull(nodes))
             return;
 
@@ -21,12 +24,15 @@ class TreePrinter {
         TreePrinter.printWhitespaces(firstSpaces);
 
         List<BinaryNode<T>> newNodes = new ArrayList<BinaryNode<T>>();
-        for (BinaryNode<T> node : nodes) {
-            if (node != null) {
+        for (BinaryNode<T> node : nodes) 
+        {
+            if (node != null) 
+            {
                 System.out.print(node.data);
                 newNodes.add(node.left);
                 newNodes.add(node.right);
-            } else {
+            } else 
+            {
                 newNodes.add(null);
                 newNodes.add(null);
                 System.out.print(" ");
@@ -36,10 +42,13 @@ class TreePrinter {
         }
         System.out.println("");
 
-        for (int i = 1; i <= endgeLines; i++) {
-            for (int j = 0; j < nodes.size(); j++) {
+        for (int i = 1; i <= endgeLines; i++) 
+        {
+            for (int j = 0; j < nodes.size(); j++) 
+            {
                 TreePrinter.printWhitespaces(firstSpaces - i);
-                if (nodes.get(j) == null) {
+                if (nodes.get(j) == null) 
+                {
                     TreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
                     continue;
                 }
@@ -58,32 +67,33 @@ class TreePrinter {
 
                 TreePrinter.printWhitespaces(endgeLines + endgeLines - i);
             }
-
             System.out.println("");
         }
 
         printNodeInternal(newNodes, level + 1, maxLevel);
     }
 
-    private static void printWhitespaces(int count) {
+    private static void printWhitespaces(int count) 
+    {
         for (int i = 0; i < count; i++)
             System.out.print(" ");
     }
 
-    private static <T extends Comparable<?>> int maxLevel(BinaryNode<T> node) {
+    private static <T extends Comparable<?>> int maxLevel(BinaryNode<T> node) 
+    {
         if (node == null)
             return 0;
 
         return Math.max(TreePrinter.maxLevel(node.left), TreePrinter.maxLevel(node.right)) + 1;
     }
 
-    private static <T> boolean isAllElementsNull(List<T> list) {
-        for (Object object : list) {
+    private static <T> boolean isAllElementsNull(List<T> list) 
+    {
+        for (Object object : list) 
+        {
             if (object != null)
                 return false;
         }
-
         return true;
     }
-
 }
